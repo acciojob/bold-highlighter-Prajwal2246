7999f7303744098c108ccf1d288954637143d3e4
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-01-01 10:55:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+const strongEle = document.querySelectorAll("strong");
 function highlight() {
     //Write your code here
 
-
+strongEle.forEach(ele=>{
+	
+	ele.style.color=rgb(0,128,0)
+})
 }
 
 
 function return_normal() {
     //Write your code here
+strongEle.forEach(ele=>{
+	
+	ele.style.color=rgb(0,128,0)
+})
 
     
 }
